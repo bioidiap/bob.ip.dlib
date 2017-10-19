52b1f3f13d7b9535ff8e57c88826f7cd3eae9bb8
Olegs NIKISINS <onikisins@italix03.idiap.ch>
2017-10-19 09:02:04 +0200

    Added the unit test for negative coordinates case

--- bob/ip/dlib/test/test.py
+++ bob/ip/dlib/test/test.py
@@ -9,6 +9,10 @@ import numpy as np
 
 from .. import FaceDetector
 
+import pkg_resources
+
+import bob.io.base
+
 def test_face_detector():
     """
     Test FaceDetector class.
@@ -25,3 +29,18 @@ def test_face_detector():
     result = FaceDetector().detect_single_face(image)
 
     assert result is None
+
+    # test on the actual image:
+    test_file = pkg_resources.resource_filename('bob.ip.dlib', 'data/test_image.hdf5')
+
+    f = bob.io.base.HDF5File(test_file) #read only
+    image = f.read('image') #reads integer
+    del f
+
+    result = FaceDetector().detect_single_face(image)
+
+    assert result[0].topleft == (0, 236)
+
+    assert result[0].bottomright == (84, 312)
+
+
