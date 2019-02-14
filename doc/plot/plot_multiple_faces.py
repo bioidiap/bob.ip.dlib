6cc7f6c0fb8c68aad73d55f08a3307f612c18629
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2019-02-14 23:24:39 +0100

    Fix plots lines disappearing

--- doc/plot/plot_multiple_faces.py
+++ doc/plot/plot_multiple_faces.py
@@ -2,14 +2,11 @@ import bob.io.base
 import bob.io.image
 import bob.io.base.test_utils
 import bob.ip.dlib
-import pkg_resources
-import os
 from matplotlib import pyplot
+from matplotlib.patches import Rectangle
 import bob.ip.draw
 import bob.ip.facedetect
 
-#print "###################################"
-#print os.path.join(pkg_resources.resource_filename(__name__, 'data'), 'multiple-faces.jpg')
 
 # detect multiple bob
 bob_color_image = bob.io.base.load(bob.io.base.test_utils.datafile('multiple-faces.jpg', 'bob.ip.dlib'))
@@ -20,19 +17,18 @@ dlib_color_image = bob.io.base.load(bob.io.base.test_utils.datafile('multiple-fa
 dlib_bounding_box, _ = bob.ip.dlib.FaceDetector().detect_all_faces(dlib_color_image)
 
 # create figure
-for b in bob_bounding_box:
-    bob.ip.draw.box(bob_color_image, b.topleft, b.size, color=(255, 0, 0))
-
-for b in dlib_bounding_box:
-    bob.ip.draw.box(dlib_color_image, b.topleft, b.size, color=(255, 0, 0))
-
 ax = pyplot.subplot(1, 2, 1)
-ax.set_title("Dlib")
+ax.set_title("bob.ip.dlib")
 pyplot.imshow(bob.io.image.to_matplotlib(dlib_color_image).astype("uint8"))
 pyplot.axis('off')
 
+for b in dlib_bounding_box:
+    ax.add_patch(Rectangle(b.topleft[::-1], b.size[1], b.size[0], edgecolor='r', facecolor='none'))
+
 ax = pyplot.subplot(1, 2, 2)
-ax.set_title("Bob")
+ax.set_title("bob.ip.facedetect")
 pyplot.imshow(bob.io.image.to_matplotlib(bob_color_image).astype("uint8"))
 pyplot.axis('off')
 
+for b in bob_bounding_box:
+    ax.add_patch(Rectangle(b.topleft[::-1], b.size[1], b.size[0], edgecolor='r', facecolor='none'))
